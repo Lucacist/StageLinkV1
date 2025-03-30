3906162112710ec36bd48151c7b6005e4ac70440
Your Name <leo.martin1@viacesi.fr>
2025-03-30 16:16:08 +0200

    Remplacement de 'Src' par 'src' dans l'index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 
 require_once ROOT_PATH . '/src/config/config.php';
 require_once ROOT_PATH . '/vendor/autoload.php';
-require_once ROOT_PATH . '/Src/Utils/UrlHelper.php';
+require_once ROOT_PATH . '/src/Utils/UrlHelper.php';
 
 $public_routes = ['login', 'logout'];
 
